eb8843221a75192bea831a414a69970ac7c2860d
Thibault <thibvollet@gmail.com>
2024-12-16 15:56:26 +0100

    📦 NEW: First commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page principale</title>
+</head>
+<body>
+    <header>
+        <!-- Logo -->
+        <a href="/"><img src="" alt=""></a>
+        <!-- Menu navigation -->
+        <nav>
+            <ul>
+                <li><a href="/">Accueil</a></li>
+                <li><a href="/clients">Clients</a></li>
+                <li><a href="/contacts">Contacts</a></li>
+                <li><a href="/projets">Projets</a></li>
+                <li><a href="/clients">Clients</a></li>
+            </ul>
+        </nav>
+        <!-- Endroit pour la connexion -->
+        <div class="login"></div>
+    </header>
+
+    <main>
+        <h1>Page principale</h1>
+    </main>
+</body>
+</html>
